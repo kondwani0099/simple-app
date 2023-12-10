ccb0b6655963356a83ccd2f39ecb3126d6cb3fb8
Kondwani Nyirenda <101860589+kondwani0099@users.noreply.github.com>
2023-12-10 17:04:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return render_template('welcome.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
